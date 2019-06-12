1f3650cd6ef85116a64ea175bbc8f832e721b2f3
wangnig <1126918543@qq.com>
2019-06-12 16:39:10 +0800

    提交mall

--- components/ShopCard.vue
+++ components/ShopCard.vue
@@ -1,51 +1,106 @@
 <template>
-  <v-card  class="black--text">
-    <v-img src="https://cdn.vuetifyjs.com/images/cards/desert.jpg" aspect-ratio="2.75"></v-img>
-    <v-layout>
-      <v-flex>
-        <v-card-title>
-          <div>
-            <div class="headline">
-              {{shop.name}}
-              <span class="body-1">
-                <v-icon small>place</v-icon>
-                {{shop.address.name}}
-              </span>
+  <v-hover>
+    <v-card class="black--text" slot-scope="{ hover }">
+      <v-img src="https://cdn.vuetifyjs.com/images/cards/desert.jpg" aspect-ratio="1.618">
+      </v-img>
+      <v-layout>
+        <v-flex>
+          <v-card-title>
+            <div>
+              <div class="headline">
+                {{shop.name}}
+              </div>
+              <div style="display: flex">
+                <v-rating :value="shop.rating" color="amber" dense half-increments readonly size="14"></v-rating>
+                <div class="ml-2 grey--text text--darken-2">
+                  <span>{{ shop.rating }}</span>
+                  <span>({{ shop.review }})</span>
+                </div>
+              </div>
             </div>
-          </div>
-        </v-card-title>
-        <v-card-text>
-          <v-layout align-center>
-              <v-icon>star</v-icon>
-              {{shop.description}}
-          </v-layout>
-        </v-card-text>
-      </v-flex>
-    </v-layout>
-    <v-divider light></v-divider>
-    <v-card-actions class="pa-3">
-      <v-layout align-center>
-        <div>
-          <v-icon small>class</v-icon>
-          {{shop.type}}
-        </div>
-        <v-spacer/>
-        <v-btn @click="bubleData()" icon>
-          <v-icon>arrow_forward</v-icon>
-        </v-btn>
+          </v-card-title>
+          <v-card-text>
+            {{shop.description}}
+          </v-card-text>
+        </v-flex>
       </v-layout>
-    </v-card-actions>
-  </v-card>
+      <v-divider light></v-divider>
+      <v-card-actions class="pa-3">
+        <v-layout align-center>
+          <div>
+            <v-icon small>class</v-icon>
+            {{shop.type}}
+          </div>
+          <v-spacer />
+          <span class="body-1">
+            <v-icon small>place</v-icon>
+            {{shop.address.name}}
+          </span>
+        </v-layout>
+      </v-card-actions>
+      <v-expand-transition>
+        <div v-if="hover"
+          class="d-flex transition-fast-in-fast-out darken-2 v-card--reveal display-3 white--text"
+          style="height: 100%;">
+          <v-list id="list" three-line style="height:100%; width: 100%;overflow-y: scroll;">
+            <template v-for="(item, index) in shop.items">
+              <v-subheader v-if="item.header" :key="item.header">
+                <v-layout justify-space-between>
+                  <div>
+                    <v-icon>thumb_up</v-icon>
+                    {{ item.header }}
+                  </div>
+                  <v-btn @click="bubleData()" color="light-blue" outline round style="height:100%;">
+                    进店
+                  </v-btn>
+                </v-layout>
+              </v-subheader>
+
+              <v-divider v-else-if="item.divider" :key="index" :inset="item.inset"></v-divider>
+
+              <v-list-tile v-else :key="item.title" avatar @click="">
+                <v-list-tile-avatar>
+                  <img :src="item.avatar">
+                </v-list-tile-avatar>
+
+                <v-list-tile-content>
+                  <v-list-tile-title v-html="item.title"></v-list-tile-title>
+                  <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+                </v-list-tile-content>
+              </v-list-tile>
+            </template>
+          </v-list>
+          <!-- <v-btn @click="bubleData()" flat dark style="height:100%; width:100%;font-size:0.8em;">
+            进店
+          </v-btn> -->
+        </div>
+      </v-expand-transition>
+    </v-card>
+  </v-hover>
 </template>
+
 <script>
-export default {
-  props: ["shop"],
-  methods: {
-    bubleData() {
-      this.$emit("chosen", this.shop);
+  export default {
+    props: ["shop"],
+    methods: {
+      bubleData() {
+        this.$emit("chosen", this.shop);
+      }
     }
-  }
-};
+  };
+
 </script>
 
-
+<style>
+  .v-card--reveal {
+    align-items: start;
+    bottom: 0;
+    justify-content: center;
+    opacity: 0.95;
+    position: absolute;
+    width: 100%;
+  }
+  #list::-webkit-scrollbar{
+    display:none;
+  }
+</style>
--- pages/yummy/customer/mall.vue
+++ pages/yummy/customer/mall.vue
@@ -1,70 +1,134 @@
 <template>
-    <v-app id="home">
-        <Header v-on:located="receiveLocation"></Header>
-        <v-content>
-            <v-container  grid-list-lg>
-                <v-layout row wrap fill-height>
-                    <v-flex v-for="(shop,index) in shops" v-bind:key="index" lg3 md4 xs12>
-                        <Card :shop="shop" v-on:chosen="goTo"></Card>
-                    </v-flex>
-                </v-layout>
-            </v-container>
-        </v-content>
-    </v-app>
+  <v-app id="home">
+    <Header v-on:located="receiveLocation"></Header>
+    <v-content>
+      <v-container grid-list-lg>
+        <v-layout row wrap fill-height>
+          <v-flex v-for="(shop,index) in shops" v-bind:key="index" lg3 md4 xs12>
+            <Card :shop="shop" v-on:chosen="goTo"></Card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import Card from "@/components/ShopCard";
-import Header from "@/components/HeaderC";
-import axios from "@/plugins/axios";
-import { distanceFunc } from "../../../utils/util";
+  import Card from "@/components/ShopCard";
+  import Header from "@/components/HeaderC";
+  import axios from "@/plugins/axios";
+  import {
+    distanceFunc
+  } from "../../../utils/util";
+  import Mock from "mockjs"
 
-export default {
+  export default {
     components: {
-        Card,
-        Header
+      Card,
+      Header
     },
     data() {
-        return {
-            shops: []
-        };
+      return {
+        shops: []
+      };
     },
     methods: {
-        receiveLocation(address) {
-            axios.get("/api/shops").then(res => {
-                this.shops = res.data.filter(
-                    shop =>
-                        distanceFunc(
-                            shop.address.lng,
-                            shop.address.lat,
-                            address.lng,
-                            address.lat
-                        ) < 100
-                );
-            });
-            this.shops = this.shops.filter(shop => {
-                let dis = distanceFunc(
-                    shop.address.lng,
-                    shop.address.lat,
-                    address.lng,
-                    address.lat
-                );
-                if (dis < 100) {
-                    return true;
-                } else return false;
-            });
-        },
-        goTo(shop) {
-            window.location.href = "/yummy/customer/shop?shopId=" + shop.id;
-        }
-    }
-    // created() {
-    //     let vue = this;
+      receiveLocation(address) {
+        // axios.get("/api/shops").then(res => {
+        //     this.shops = res.data.filter(
+        //         shop =>
+        //             distanceFunc(
+        //                 shop.address.lng,
+        //                 shop.address.lat,
+        //                 address.lng,
+        //                 address.lat
+        //             ) < 100
+        //     );
+        // });
+        // this.shops = this.shops.filter(shop => {
+        //     let dis = distanceFunc(
+        //         shop.address.lng,
+        //         shop.address.lat,
+        //         address.lng,
+        //         address.lat
+        //     );
+        //     if (dis < 100) {
+        //         return true;
+        //     } else return false;
+        // });
+        let random = Mock.Random;
+        let shops = []
+        for (let i = 0; i < 10; i++) {
+          const element = {
+            name: random.cword(2, 8),
+            address: {
+              name: random.city()
+            },
+            description: random.cparagraph(1, 5),
+            type: random.ctitle(2, 4),
+            rating: random.float(0, 4.9, 1, 1),
+            review: random.integer(0, 1000),
+            items: [{
+                header: '店铺推荐'
+              },
+              {
+                avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+                title: 'Brunch this weekend?',
+                subtitle: "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
+              },
+              {
+                divider: true,
+                inset: true
+              },
+              {
+                avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+                title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
+                subtitle: "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend."
+              },
+              {
+                divider: true,
+                inset: true
+              },
+              {
+                avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+                title: 'Oui oui',
+                subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
+              },
+              {
+                divider: true,
+                inset: true
+              },
+              {
+                avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
+                title: 'Birthday gift',
+                subtitle: "<span class='text--primary'>Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?"
+              },
+              {
+                divider: true,
+                inset: true
+              },
+              {
+                avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
+                title: 'Recipe to try',
+                subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos."
+              }
+            ]
+          }
+          shops.push(element);
 
-    // }
-};
+        }
+        this.shops = shops;
+      },
+      goTo(shop) {
+        window.location.href = "/yummy/customer/shop?shopId=" + shop.id;
+      }
+    },
+    created: function () {
+      this.receiveLocation();
+    }
+  };
+
 </script>
 
 <style>
 </style>
-
